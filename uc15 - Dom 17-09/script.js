f4069d7dd8da31268b824e1551f859fb6b0e7fc4
saravitoriasalvianoo <saravitoriasalvianoo@gmail.com>
2026-09-17 10:21:10 -0300

    Add files via upload

---
+++ uc15 - Dom 17-09/script.js
@@ -0,0 +1,9 @@
+function mudarTexto(params) {
+    document.getElementById
+    ("titulo").textContent = "Você clicou no botão!";
+}
+
+function mudarCor () {
+    document.getElementById("mensagem").style.color =
+    "pink";
+}
